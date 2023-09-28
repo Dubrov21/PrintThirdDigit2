79b8884cce58293e2a26ce62a5bf03dcf0679a4d
Dubrov21 <vald5red@yandex.ru>
2023-09-28 22:24:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Second_Digit_Number
+{
+    public class Answer
+    {
+        static void PrintThirdDigit(int number)
+        {
+            string numStr = number.ToString();
+            if (numStr.Length < 3)
+            {
+                Console.WriteLine("Третьей цифры нет");
+            }
+            else
+            {
+                char num = numStr[2];
+                Console.WriteLine(num);
+            }
+        }
+
+
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Введите число");
+            int number;
+
+            if (args.Length >= 1)
+            {
+                number = int.Parse(args[0]);
+            }
+            else
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            PrintThirdDigit(number);
+        }
+    }
+}
